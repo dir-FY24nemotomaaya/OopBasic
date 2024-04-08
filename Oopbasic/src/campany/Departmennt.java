079ff301e621fb3d216678740a0412b20df90226
MayaNemoto(根本真綾) <maaya.nemoto.m639@dirbato.co.jp>
2024-04-08 16:57:48 +0900

    L3chp4.end.commit

---
+++ Oopbasic/src/campany/Departmennt.java
@@ -0,0 +1,5 @@
+package campany;
+
+public class Departmennt {
+
+}
--- Oopbasic/src/campany/ObjectSample.java
+++ Oopbasic/src/campany/ObjectSample.java
@@ -3,7 +3,21 @@ package campany;
 public class ObjectSample {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+     // インスタンスの作成
+        var employee = new Employee("鈴木", "営業部", "課長", 100);
+
+        // インスタンスメソッドの呼び出し
+        employee.report();
+        employee.report(2);
+        
+        // 1行空ける
+        System.out.println("");
+
+        // インスタンスの作成
+        var engineer = new Employee("田中", "開発部", "一般社員", 88);
+
+        // インスタンスメソッドの呼び出し
+        engineer.report();
 
     }
 
