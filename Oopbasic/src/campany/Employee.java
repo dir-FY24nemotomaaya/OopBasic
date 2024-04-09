ee476e4d3778be0779f4ba11a5e6066dd15fa61e
MayaNemoto(根本真綾) <maaya.nemoto.m639@dirbato.co.jp>
2024-04-09 11:13:24 +0900

    L3Chp5.end.commit

--- Oopbasic/src/campany/Employee.java
+++ Oopbasic/src/campany/Employee.java
@@ -3,13 +3,13 @@ package campany;
 public class Employee {
     
     // フィールド(属性)の定義
-    String name;
-    String department;
-    String position;
-    int employeeId;
+    protected final String name;
+    private final Departmennt department;
+    private final String position;
+    private final int employeeId;
 
     // コンストラクターの定義＝インスタンスの初期化
-    public Employee(String name, String department, String position, int employeeId) {
+    public Employee(String name, Departmennt department, String position, int employeeId) {
         this.name = name;
         this.department = department;
         this.position = position;
@@ -24,6 +24,12 @@ public class Employee {
     // 報告メソッド（オーバーロード）
     public void report() {
         report(1);
+    }
+    
+ // 会議に参加するメソッド
+    public void joinMeeting() {
+        department.meeting();
+        System.out.println("→上記の会議に参加します。部署：" + department.getName() + "名前：" + name);
     }
 
 }
--- Oopbasic/src/campany/ObjectSample.java
+++ Oopbasic/src/campany/ObjectSample.java
@@ -4,7 +4,8 @@ public class ObjectSample {
 
     public static void main(String[] args) {
      // インスタンスの作成
-        var employee = new Employee("鈴木", "営業部", "課長", 100);
+        var department = new Departmennt("営業部", "xx", 1000000);
+        var employee = new Employee("鈴木", department, "課長", 100);
 
         // インスタンスメソッドの呼び出し
         employee.report();
@@ -14,10 +15,12 @@ public class ObjectSample {
         System.out.println("");
 
         // インスタンスの作成
-        var engineer = new Employee("田中", "開発部", "一般社員", 88);
+        var Departmennt = new Departmennt("開発部", "yy", 0);
+        var engineer = new Employee("田中", Departmennt, "一般社員", 88);
 
         // インスタンスメソッドの呼び出し
         engineer.report();
+        engineer.joinMeeting();
 
     }
 
