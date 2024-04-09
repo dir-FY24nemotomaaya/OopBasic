82d20b509fc65b95dd2eb8317ac1d1fd0207b68d
MayaNemoto(根本真綾) <maaya.nemoto.m639@dirbato.co.jp>
2024-04-09 14:31:37 +0900

    L4Chp2.end.commit

--- Oopbasic/src/campany/ObjectSample.java
+++ Oopbasic/src/campany/ObjectSample.java
@@ -3,7 +3,7 @@ package campany;
 public class ObjectSample {
 
     public static void main(String[] args) {
-     // インスタンスの作成
+     // インスタンスの作成（鈴木さんver）
         var department = new Departmennt("営業部", "xx", 1000000);
         var employee = new Employee("鈴木", department, "課長", 100);
 
@@ -16,14 +16,26 @@ public class ObjectSample {
         // 1行空ける
         System.out.println("");
 
-        // インスタンスの作成
+        // インスタンスの作成(田中さんver)
         var Departmennt = new Departmennt("開発部", "yy", 0);
         var engineer = new Engineer("田中", Departmennt, "一般社員", 88, "Java");
 
         // インスタンスメソッドの呼び出し
-        engineer.report();
-        engineer.joinMeeting(); 
-        engineer.developSoftware(); //追記
+        engineer.report(); //基底クラスのメソッドも呼び出し可能
+        engineer.joinMeeting();  //オーバーライドの結果を出力
+        engineer.developSoftware(); //派生クラスで定義したメソッド
+        
+     // ポリモーフィズムの確認
+        Employee projectManager = new Engineer("佐藤", Departmennt, "PM", 99, "Java");
+        //varではなくEmployeeクラスを指定してEmployeeオブジェクト代入
+        
+        // インスタンスメソッドの呼び出し
+        projectManager.report();
+        projectManager.joinMeeting();
+        if (projectManager instanceof Engineer) {
+            // developSoftwareメソッドを呼び出す
+            ((Engineer) projectManager).developSoftware();
+        }
 
     }
 
